8a36aee3daa7fbaae8138f1902b6c785ce2da251
sciborbudkowski <scibor.budkowski@gmail.com>
2023-01-25 14:02:48 +0100

    Apply .gitignore

--- lib/secrets.dart
+++
@@ -1,4 +0,0 @@
-const unsplashApiAccessKey = 'n20EpZ-8zalEjmSOCvFzyuBrsDA734SjHqR0LJ_l9gY';
-const unsplashApiSecretKey = '3DHxw4SGZsgempM2wvowccAvSsjzQ2eskjMTTIJtHRk';
-const pirateWeatherApiKey = 'Qmhw25GQ2R87zgoWjI1wL7KCIaLsSoXsat1OazC0';
-const openWeatherApiKey = '1267ccba117cbc3938e51a0386683f80';
